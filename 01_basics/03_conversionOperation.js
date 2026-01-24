dbe03cf54c848d55fc9098af790a97aeea2e93ab
aankitajain <83642230+aankitajain@users.noreply.github.com>
2026-01-24 04:37:01 +0000

    moreonconversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -54,3 +54,31 @@ let someNumber = 33
 let string = String(someNumber)
 console.log(string)
 console.log(typeof (string))
+
+///////////Operation/////////////
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// let str1 = "hello"
+// let str2 = "ankita"
+// let str3 = str1 + str2
+// console.log(str3);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + "2" + 2);
+// console.log(1 + 2 + "2");
+console.log(+true);
+
+let num1 ,num2 ,num3
+num1 = num2 = num3 = 2+2
+console.log(num1);
+
+let gamecounter = 100
+++gamecounter 
+console.log(gamecounter);
+//prefix and postfix value inteview question  
+
+
+
