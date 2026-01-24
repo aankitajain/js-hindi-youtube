bff7739de03dc755350908d04e492119d86f8844
aankitajain <83642230+aankitajain@users.noreply.github.com>
2026-01-24 03:34:53 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+
+let name = "aj"
+let age = 14
+let isLoggedIn = false
+//let state;  this is undefined
+//let state = null
+
+//primitive datatypes
+// number => 2 to power 53
+//bigint 
+//string => single or double quote any
+//boolean=>  true or false
+//null => standalone value
+//undefined  => value is not assigned
+//symbol => unique
+
+//non primitive datatypes
+//interview question  null is an object
+//object
+
+console.log(typeof "aj")
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof null);          //null ka type is object
+console.log(typeof undefined);    //undefined ka type is undefined
