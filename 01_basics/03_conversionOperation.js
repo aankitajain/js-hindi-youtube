35921c84a86c5de23529834db2a96934ce709d6e
aankitajain <83642230+aankitajain@users.noreply.github.com>
2026-01-24 04:11:15 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,56 @@
+let score = "33aa"
+
+console.log(typeof score);
+
+
+//I want value in number not string
+let valueInNumber = Number(score)
+console.log(typeof (score))
+console.log(typeof (valueInNumber))
+console.log(valueInNumber)
+//all data type has capital format
+
+let score1 = null
+
+console.log(typeof score);
+
+
+
+let valueInNumber1 = Number(score1)
+console.log(typeof (score1))
+console.log(typeof (valueInNumber1))
+console.log(valueInNumber1)
+
+let score2 = undefined
+
+console.log(typeof score2);
+
+
+
+let valueInNumber2 = Number(score2)
+console.log(typeof (score2))
+console.log(typeof (valueInNumber2))
+console.log(valueInNumber2)
+
+//  string can easily be converted to number    "33"  => 33
+// "33abc"  (unable to convert this to Number ) so this gives return type as NaN
+// true => 1  ; false => 0 
+
+let isLoggedIn 
+
+//want to convert 1 to boolen here
+let boolean = Boolean(isLoggedIn);
+console.log(boolean)
+
+//1 =? true ; 0=>false
+// " " => false
+//"ab" => true
+
+
+////////convert variable to string
+
+let someNumber = 33
+
+let string = String(someNumber)
+console.log(string)
+console.log(typeof (string))
