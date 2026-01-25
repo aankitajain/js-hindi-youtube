3ad4116e518df1888c8838318a2c3a5d39613d9e
aankitajain <83642230+aankitajain@users.noreply.github.com>
2026-01-25 09:24:26 +0000

    arrays

---
+++ 01_basics/02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+//array
+
+const myArray = [0,1,2,3,4,5,true,"ankita"]
+const myHeros = ["shaktiman","naagraj"]
+const myArr2 = new Array(1,2,3,4)
+
+console.log(myArray[1]);
+
+//Array methods
+
+myArray.push(6)
+console.log(myArr2);
+
