bbb949d70af96b7283760c883d7cca066bf3087e
aankitajain <83642230+aankitajain@users.noreply.github.com>
2026-01-25 21:46:00 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,13 +1,33 @@
 //array
 
 const myArray = [0,1,2,3,4,5,true,"ankita"]
-const myHeros = ["shaktiman","naagraj"]
-const myArr2 = new Array(1,2,3,4)
+// const myHeros = ["shaktiman","naagraj"]
+// const myArr2 = new Array(1,2,3,4)
 
-console.log(myArray[1]);
+// console.log(myArray[1]);
 
-//Array methods
+// //Array methods
 
-myArray.push(6)
-console.log(myArr2);
+// myArray.push(23,24,27,27,28,29)
+// myArray.pop()
+//console.log(myArray.indexOf(8));
+
+const newArr = myArray.join()
+// console.log(newArr);
+// console.log(typeof newArr);
+// console.log(typeof myArray);
+
+//slice,splice
+
+console.log("A ", myArray);
+
+const myn1 = myArray.slice(1,3)
+console.log(myn1);
+console.log("B" ,myArray );
+
+const myn2 = myArray.splice(1,3)
+console.log(myn2);
+
+
+
 
