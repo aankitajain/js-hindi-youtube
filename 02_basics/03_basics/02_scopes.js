beab426adf4b29a241fcc0fc9164b158f0ca4db6
aankitajain <83642230+aankitajain@users.noreply.github.com>
2026-01-26 23:20:27 +0000

    scope

--- 02_basics/03_basics/02_scopes.js
+++ 02_basics/03_basics/02_scopes.js
@@ -6,11 +6,57 @@ if (true) {
    let a = 10
 const b = 20
 
-console.log("INNER: " , a);
+//console.log("INNER: " , a);
     
 }
 }
 
 //console.log(a);
 //console.log(b);
-console.log(a);
+//console.log(a);
+
+function one(){
+    const username = "ankita"
+
+    function two(){
+        const website = "youtube"
+        //console.log(username);
+        
+    }
+   // console.log(website);
+two()
+    
+}    
+one()
+
+if (true) 
+    {
+    const username = "aj"
+    if (username==="aj") 
+    {
+        const website = " youtube"
+        //console.log(username + website);
+        
+        
+    }
+    //console.log(website);
+    
+    }
+    //console.log(username);
+
+    //++++++++++++++++interesting ++ 
+
+    function addone(num)          //1st way of writing function
+    {
+    return num+1
+    
+   
+
+    }
+     addone(5)
+
+    const addTwo = function(num)  //2nd way of writing function
+    {
+        return num+2
+    }
+    addTwo(5)                     //hoisting concept when function written this way 
