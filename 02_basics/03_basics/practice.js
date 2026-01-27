5d58bf3ead20678995bf263ed9b36c1e0734beab
aankitajain <83642230+aankitajain@users.noreply.github.com>
2026-01-27 08:40:04 +0000

    arrows

--- 02_basics/03_basics/practice.js
+++ 02_basics/03_basics/practice.js
@@ -8,14 +8,20 @@ const user =
     welcomemsg: function()
     {
    console.log(`${this.username} , welcome to website`);
+   console.log(this);
+   
+   
     
     }
    
 }
 //welcomemsg(user)      //why cant i call function here question to explore:answer- coz it is not a standalone function rather inside a object
 user.welcomemsg()
+
 user.username ="samu"
-console.log(`${this.username} , welcome to website`);
+console.log(this);     //node env k andar this empty {} ko represent krra
+console.log(`${this.username} , welcome to website again`);
+user.username ="akash"
 
 user.welcomemsg()
 
