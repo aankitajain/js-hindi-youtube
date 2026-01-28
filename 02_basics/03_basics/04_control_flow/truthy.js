cfa63ecafa9d2c99b66a2407673187239533ae87
aankitajain <83642230+aankitajain@users.noreply.github.com>
2026-01-28 02:01:44 +0000

    truthy

---
+++ 02_basics/03_basics/04_control_flow/truthy.js
@@ -0,0 +1,44 @@
+const userEmail = []
+
+// if (userEmail) {
+//     console.log("got email");
+    
+    
+// }
+// else{
+//     console.log("dont");
+    
+// }
+
+//false values : false , 0 , -0 , BigInt 0n , "", null, undefined, NaN  inke alava saare true values
+//truthy values  : true ,1 ,"0", 'false' ," ", [] , {}, function(){}
+// //  (string k andar true ki value ho jti hai), 
+
+// if(userEmail.length===0){
+//     console.log("Array is empty");
+    
+// }
+
+// const emptyobj = {}
+
+// if (Object.keys(emptyobj).length===0) {
+//     console.log("Object is empty");
+    
+    
+// }
+//Nullish Coalescing Operator (??) : null undefined
+
+// let val1;
+// //val1 =  5?? 10
+// val1 =  null ?? 10
+// val1 = undefined ?? 15
+// val1 = null?? 10??20
+// console.log(val1);
+
+// //Terniary Operator
+// condition? true:false
+
+const iceTeaPrice =100
+iceTeaPrice<=80 ? console.log("less than 80" ) : console.log("more than 80");
+
+
