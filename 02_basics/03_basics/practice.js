3e1db6e4b03d295161f9ab897987833b5d446d2a
aankitajain <83642230+aankitajain@users.noreply.github.com>
2026-01-28 01:09:04 +0000

    prac

--- 02_basics/03_basics/practice.js
+++ 02_basics/03_basics/practice.js
@@ -25,3 +25,33 @@ user.username ="akash"
 
 user.welcomemsg()
 
+
+/////
+//singelton when made by constructor singelton banega when by literal singelton ni banega
+
+Object.create   // singeltion constructor k through
+const mysym = Symbol("key1")
+const jsuser = { "name" : "Hitesh",
+    age:10,
+    location: "Jaipur",
+     [mysym]: "mykey1",
+    "full name" : "ajainaa",
+    email: "a@com",
+    isLoggedin: false
+}
+console.log(jsuser["full name"]);
+console.log(jsuser[mysym]);
+console.log(typeof  [mysym]);
+
+
+const mynewarray =[200,400,500]
+function returnsecond(getarr){
+    return getarr[2]
+
+}
+console.log(returnsecond.mynewarray([200,100]));
+
+
+
+
+
