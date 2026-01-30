5705dbe3350f1a678fc88550b4663487e1d96447
aankitajain <83642230+aankitajain@users.noreply.github.com>
2026-01-30 08:07:37 +0000

    eight

--- 02_basics/03_basics/05_iterations/eight.js
+++ 02_basics/03_basics/05_iterations/eight.js
@@ -1,11 +1,35 @@
 const mymethodreduce = [1,2,3]
 
 
- const myTotal = mymethodreduce.reduce( function ( accumalator, currentval) {
-    console.log(`acc: ${accumalator} and currentvalue : ${currentval}`);
+//  const myTotal = mymethodreduce.reduce( function ( accumalator, currentval) {
+//     console.log(`acc: ${accumalator} and currentvalue : ${currentval}`);
     
-return   accumalator +currentval
+// return   accumalator +currentval
 
- } ,3)
- console.log(myTotal);
+//  } ,3)
+
+// const myTotal = mymethodreduce.reduce( (acc,currval) =>  acc+currval ,3)
+
+
+//  console.log(myTotal);
+
+const shoppingCart = [
+    {
+       itemName: "js course",
+       price: 299 
+    },
+     {
+       itemName: "py course",
+       price: 399 
+    },
+     {
+       itemName: "data course",
+       price: 499 
+    },
+]
+
+const pricetoPay = shoppingCart.reduce( (acc, item) => (acc + item.price),0 )
+
+console.log(pricetoPay);
+
  
