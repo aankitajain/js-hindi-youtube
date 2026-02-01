f3b1d90a24f3398cd6acdbf47bb1ec9c7e6bcceb
aankitajain <83642230+aankitajain@users.noreply.github.com>
2026-02-01 23:20:00 +0000

    pro

--- 09_advance/promises.js
+++ 09_advance/promises.js
@@ -29,3 +29,60 @@ new Promise(function(resolve,reject){
 )
 
 
+const promiseThree = new Promise(function(resolve,reject){
+setTimeout(function(){
+resolve({username:'chai',email:'chai@chai.com'})
+},1000)
+})
+
+promiseThree.then(function(user){
+console.log(user);
+
+})
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username:'hitesh',password:'123'})
+            
+        }else{
+            reject('ERROR : Something went wrong')
+        }
+    },1000)
+})
+
+ promiseFour
+ .then( (user) => {
+    console.log(user);
+    return user.username
+    
+}).then((username) => {
+console.log(username);
+
+}).catch(function(err){
+    console.log(err);
+    
+}).finally(() => console.log('The promise is either resolved or rejected'));
+
+
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username:'javascript',password:'123'})
+            
+        }else{
+            reject('ERROR:JS went wrong')
+        }
+    },1000)
+});
+
+async function consumePromiseFive() {
+    const response = await promiseFive
+    console.log(response);
+    
+}
+consumePromiseFive()
+
+
